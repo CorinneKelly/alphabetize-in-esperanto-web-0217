064aeac86d1a0c5e9a69193d68fc89f989fb9684
Corinne Kelly <corinnemariekelly@gmail.com>
2017-02-06 00:28:20 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
-def alphabetize(arr)
-  # code here
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+def alphabetize(array)
+
+  array.sort_by do |phrase|
+    phrase.split("").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
+
 end
